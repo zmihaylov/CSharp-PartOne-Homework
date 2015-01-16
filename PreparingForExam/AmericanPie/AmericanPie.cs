4fd3fb85be75e517b254a7b6de15835b7f971ff5
Zlatimir Mihaylov <zlatimirmihailov@gmail.com>
2015-01-16 19:59:38 +0200

    Add on

---
+++ PreparingForExam/AmericanPie/AmericanPie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class AmericanPie
+{
+    static void Main()
+    {
+        long firstNominator = long.Parse(Console.ReadLine());
+        long firstDenominator = long.Parse(Console.ReadLine());
+
+        long secondNominator = long.Parse(Console.ReadLine());
+        long secondDenominator = long.Parse(Console.ReadLine());
+
+        long resultNominator = firstNominator * secondDenominator + secondNominator * firstDenominator;
+        long resultDenominator = firstDenominator * secondDenominator;
+
+        decimal result = resultNominator / (decimal)resultDenominator;
+
+        if (result >= 1)
+        {
+            Console.WriteLine((long)result);
+        }
+        else
+        {
+            Console.WriteLine("{0:F20}",result);
+        }
+        Console.WriteLine(resultNominator + "/" + resultDenominator);
+    }
+}
